b65507fc833808e8169d2278ae10e3d4f68012f0
Sthabhorn <sathapornp@pccth.com>
2022-12-25 23:54:27 +0700

    add go module

--- server.go
+++ server.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/gdaz/assessment/expense"
+
+	"github.com/gdaz/assessment/routers"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	expense.InitDB()
+
 	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	fmt.Println("PORT: ", os.Getenv("PORT"))
+
+	r := routers.InitRouter()
+
+	r.Run(os.Getenv("PORT"))
 }
