5c5a8c89675215aacf9b673a1e02e8ea013f0a6c
Sthabhorn <sathapornp@pccth.com>
2022-12-27 00:06:43 +0700

    list all expenses

--- expense/get.go
+++ expense/get.go
@@ -23,5 +23,30 @@ func GetExpense(c *gin.Context) {
 }
 
 func GetExpenses(c *gin.Context) {
+	fmt.Println("GetExpenses" + c.ClientIP())
 
+	stmt, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query user statment:" + err.Error()})
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Err{Message: "can't query all users:" + err.Error()})
+	}
+
+	expenses := []ExpenseBody{}
+
+	for rows.Next() {
+		e := ExpenseBody{}
+		// tags := []string{}
+		err := rows.Scan(&e.Id, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, Err{Message: "can't scan user:" + err.Error()})
+		}
+		// e.Tags = tags
+		expenses = append(expenses, e)
+	}
+
+	c.JSON(http.StatusOK, expenses)
 }
