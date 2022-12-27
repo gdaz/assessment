a040426cde9939a6e51f32191780cd0ba8a9b7de
Sthabhorn <sathapornp@pccth.com>
2022-12-27 10:40:25 +0700

    chore: get database url from environment

--- expense/database.go
+++ expense/database.go
@@ -3,18 +3,27 @@ package expense
 import (
 	"database/sql"
 	"log"
+	"os"
 )
 
 var db *sql.DB
 
 func InitDB() {
-	const url = "postgres://zcqexosc:GK-Gjf0HlysSDAe1vhfeOxwEq3p2uYlD@tiny.db.elephantsql.com/zcqexosc"
+	// const url = "postgres://zcqexosc:GK-Gjf0HlysSDAe1vhfeOxwEq3p2uYlD@tiny.db.elephantsql.com/zcqexosc"
 
 	// db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	var err error
-	db, err = sql.Open("postgres", url)
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func CloseDB() {
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("DB connection gracefully closed")
 	}
 }
