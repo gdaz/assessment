8cdc7607224326a76ed6ec4ddf6435d6d793f82e
Sthabhorn <sathapornp@pccth.com>
2022-12-27 09:55:44 +0700

    test: get all expenses and get expense by ID

--- expense/expense_integration_test.go
+++ expense/expense_integration_test.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,7 +21,7 @@ func TestAddNewExpense(t *testing.T) {
 	}`)
 	var eb ExpenseBody
 
-	res := request(http.MethodPost, "http://localhost:2565/expenses", body)
+	res := request(http.MethodPost, uri(), body)
 	err := res.Decode(&eb)
 
 	assert.Nil(t, err)
@@ -30,9 +32,45 @@ func TestAddNewExpense(t *testing.T) {
 	assert.Equal(t, 2, len(eb.Tags))
 }
 
+func TestGetExpenseByID(t *testing.T) {
+	var eb ExpenseBody
+
+	res := request(http.MethodGet, uri("1"), nil)
+	err := res.Decode(&eb)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+	assert.NotEqual(t, 0, eb.Id)
+	assert.Equal(t, "apple smoothie", eb.Title)
+	assert.Equal(t, "no discount", eb.Note)
+	assert.Equal(t, int64(89), eb.Amount)
+	assert.Equal(t, 1, len(eb.Tags))
+}
+
+func TestGetAllExpense(t *testing.T) {
+	var eb []ExpenseBody
+
+	res := request(http.MethodGet, uri(), nil)
+	err := res.Decode(&eb)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+	assert.NotEqual(t, 0, len(eb))
+}
+
+func uri(paths ...string) string {
+	host := "http://localhost:2565/expenses"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
 func request(method, url string, body io.Reader) *Response {
 	req, _ := http.NewRequest(method, url, body)
-	// req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
+	req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
 	req.Header.Add("Content-Type", "application/json")
 	client := http.Client{}
 	res, err := client.Do(req)
